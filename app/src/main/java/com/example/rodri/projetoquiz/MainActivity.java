87671d04e9a213d1b8e24b3bca3b43e4d2d1823d
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2018-10-26 12:26:13 -0400

    Motodos contagem pontuacao

--- app/src/main/java/com/example/rodri/projetoquiz/MainActivity.java
+++ app/src/main/java/com/example/rodri/projetoquiz/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.rodri.projetoquiz;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,11 +50,26 @@ public class MainActivity extends AppCompatActivity {
     RadioButton radioBtnNineFour;
     EditText editTextTen;
 
+    RadioGroup radioGroup1;
+    RadioGroup radioGroup4;
+    RadioGroup radioGroup6;
+    RadioGroup radioGroup7;
+    RadioGroup radioGroup8;
+    RadioGroup radioGroup9;
+    List<RadioGroup> listaRadioGroup = new ArrayList<>();
+    List<RadioButton> listRadioBtnCertos = new ArrayList<>();
+
+    CheckBox[][] matrizCheckBox = new CheckBox[2][4];
+    CheckBox[] vetorCheckBox = new CheckBox[5];
+
+    int pontucao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         atribuirComponenteView();
+        pontucao = 0;
     }
 
     private void atribuirComponenteView(){
@@ -86,5 +107,149 @@ public class MainActivity extends AppCompatActivity {
         radioBtnNineThree = findViewById(R.id.radio_btn_nine_three);
         radioBtnNineFour = findViewById(R.id.radio_btn_nine_four);
         editTextTen = findViewById(R.id.edit_text_ten);
+
+        radioGroup1 = findViewById(R.id.radio_group_one);
+        radioGroup4 = findViewById(R.id.radio_group_four);
+        radioGroup6 = findViewById(R.id.radio_group_six);
+        radioGroup7 = findViewById(R.id.radio_group_seven);
+        radioGroup8 = findViewById(R.id.radio_group_eight);
+        radioGroup9 = findViewById(R.id.radio_group_nine);
+        listaRadioGroup.add(radioGroup1);
+        listaRadioGroup.add(radioGroup4);
+        listaRadioGroup.add(radioGroup6);
+        listaRadioGroup.add(radioGroup7);
+        listaRadioGroup.add(radioGroup8);
+        listaRadioGroup.add(radioGroup9);
+
+        listRadioBtnCertos.add(radioBtnOneFour);
+        listRadioBtnCertos.add(radioBtnFourThree);
+        listRadioBtnCertos.add(radioBtnSixFour);
+        listRadioBtnCertos.add(radioBtnSevenFour);
+        listRadioBtnCertos.add(radioBtnEightThree);
+        listRadioBtnCertos.add(radioBtnNineOne);
+
+        matrizCheckBox[0][0] = checkBoxThreeOne;
+        matrizCheckBox[0][1] = checkBoxThreeTwo;
+        matrizCheckBox[0][2] = checkBoxThreeThree;
+        matrizCheckBox[0][3] = checkBoxThreeFour;
+        matrizCheckBox[1][0] = checkBoxFiveOne;
+        matrizCheckBox[1][1] = checkBoxFiveTwo;
+        matrizCheckBox[1][2] = checkBoxFiveThree;
+        matrizCheckBox[1][3] = checkBoxFiveFour;
+
+        vetorCheckBox[0] = checkBoxThreeOne;
+        vetorCheckBox[1] = checkBoxThreeThree;
+        vetorCheckBox[2] = checkBoxFiveOne;
+        vetorCheckBox[3] = checkBoxFiveTwo;
+        vetorCheckBox[4] = checkBoxFiveThree;
+    }
+
+
+    public void pontuacaoQuiz(View view){
+        compararRadioButton();
+//        compararCheckBox();
+//        opcoesDesabilitadas();
+//        compararEditText();
+//        Toast.makeText(getApplicationContext(), "Voce acertou: " + pontucao + "%% do quiz", Toast.LENGTH_LONG).show();
+    }
+
+    private void compararEditText(){
+        String respostaQuestionTwo = getResources().getString(R.string.edit_resposta_two);
+        String respostaQuestionTen = getResources().getString(R.string.edit_resposta_ten);
+        if (respostaQuestionTwo.equalsIgnoreCase(editTextTwo.getText().toString())){
+            pontucao += 10;
+            editTextTwo.setTextColor(getResources().getColor(R.color.resposta_certa));
+            editTextTwo.setTypeface(Typeface.DEFAULT_BOLD);
+        }else {
+            editTextTwo.setTextColor(getResources().getColor(R.color.resposta_errada));
+        }
+        if (respostaQuestionTen.equalsIgnoreCase(editTextTen.getText().toString())){
+            pontucao += 10;
+            editTextTen.setTextColor(getResources().getColor(R.color.resposta_certa));
+            editTextTen.setTypeface(Typeface.DEFAULT_BOLD);
+        }else {
+            editTextTen.setTextColor(getResources().getColor(R.color.resposta_errada));
+        }
+    }
+
+    private void compararCheckBox(){
+        for (int i = 0; i < matrizCheckBox.length; i++){
+            for (int j = 0; j < matrizCheckBox[i].length; j++){
+                if (matrizCheckBox[i][j].isChecked()){
+                    compararMatrizVetorCheckBox(i,j);
+                }
+            }
+        }
+        for (CheckBox alternativas : vetorCheckBox) {
+            if (!alternativas.isChecked()) {
+                alternativas.setTypeface(Typeface.DEFAULT_BOLD);
+                alternativas.setTextColor(getResources().getColor(R.color.resposta_certa));
+            }
+        }
+    }
+
+    private void compararMatrizVetorCheckBox(int linha, int coluna){
+        for (CheckBox alternativas : vetorCheckBox){
+            if (alternativas.equals(matrizCheckBox[linha][coluna])){
+                alternativas.setTypeface(Typeface.DEFAULT_BOLD);
+                alternativas.setTextColor(getResources().getColor(R.color.resposta_certa));
+                pontucao += 4;
+                return;
+            } else {
+                matrizCheckBox[linha][coluna].setTextColor(getResources().getColor(R.color.resposta_errada));
+            }
+        }
+    }
+
+    private void compararRadioButton(){
+        for (RadioGroup group : listaRadioGroup){
+            RadioButton respostaRadioBtn = findViewById(group.getCheckedRadioButtonId());
+            for(RadioButton button : listRadioBtnCertos){
+                if (button.equals(respostaRadioBtn)){
+                    pontucao += 10;
+                    button.setTypeface(Typeface.DEFAULT_BOLD);
+                } else {
+                    respostaRadioBtn.setTextColor(getResources().getColor(R.color.resposta_errada));
+                }
+                button.setTextColor(getResources().getColor(R.color.resposta_certa));
+            }
+        }
+    }
+
+    private void opcoesDesabilitadas(){
+        radioBtnOneOne.setEnabled(false);
+        radioBtnOneTwo.setEnabled(false);
+        radioBtnOneThree.setEnabled(false);
+        radioBtnOneFour.setEnabled(false);
+        editTextTwo.setEnabled(false);
+        checkBoxThreeOne.setEnabled(false);
+        checkBoxThreeTwo.setEnabled(false);
+        checkBoxThreeThree.setEnabled(false);
+        checkBoxThreeFour.setEnabled(false);
+        radioBtnFourOne.setEnabled(false);
+        radioBtnFourTwo.setEnabled(false);
+        radioBtnFourThree.setEnabled(false);
+        radioBtnFourFour.setEnabled(false);
+        checkBoxFiveOne.setEnabled(false);
+        checkBoxFiveTwo.setEnabled(false);
+        checkBoxFiveThree.setEnabled(false);
+        checkBoxFiveFour.setEnabled(false);
+        radioBtnSixOne.setEnabled(false);
+        radioBtnSixTwo.setEnabled(false);
+        radioBtnSixThree.setEnabled(false);
+        radioBtnSixFour.setEnabled(false);
+        radioBtnSevenOne.setEnabled(false);
+        radioBtnSevenTwo.setEnabled(false);
+        radioBtnSevenThree.setEnabled(false);
+        radioBtnSevenFour.setEnabled(false);
+        radioBtnEightOne.setEnabled(false);
+        radioBtnEightTwo.setEnabled(false);
+        radioBtnEightThree.setEnabled(false);
+        radioBtnEightFour.setEnabled(false);
+        radioBtnNineOne.setEnabled(false);
+        radioBtnNineTwo.setEnabled(false);
+        radioBtnNineThree.setEnabled(false);
+        radioBtnNineFour.setEnabled(false);
+        editTextTen.setEnabled(false);
     }
 }
